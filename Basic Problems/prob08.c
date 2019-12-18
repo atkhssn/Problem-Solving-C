00b1a75cdb3d8bc2546dac62015805cbba519fc3
atkhssn <atk.hssn2017@gmail.com>
2019-12-18 22:29:09 +0600

    Solve problem 08

---
+++ Basic Problems/prob08.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+// Write a C program to enter temperature in Celsius and convert it into Fahrenheit
+
+int main()
+{
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * (9 / 5)) + 32; // Celsius to Fahrenheit
+
+    printf("\nYou entered %.2f Celsius = %.2f Fahrenheit\n", celsius, fahrenheit);
+
+    return 0;
+}
